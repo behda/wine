1545ca9555b4d08727a61922074aa1ee178d4fd8
Stefan Dösinger <stefan@codeweavers.com>
2014-01-16 22:04:55 +0100

    wined3d: Move bitmap_data and user_memory into the resource.
    I may want to change this to keep this in the surface. Not sure yet.

--- dlls/wined3d/wined3d_private.h
+++ dlls/wined3d/wined3d_private.h
@@ -2080,7 +2080,7 @@ struct wined3d_resource
     UINT depth;
     UINT size;
     DWORD priority;
-    void *heap_memory;
+    void *heap_memory, *user_memory, *bitmap_data;
     struct list resource_list_entry;
     DWORD locations;
 
@@ -2262,7 +2262,6 @@ void wined3d_volume_upload_data(struct wined3d_volume *volume, const struct wine
 struct wined3d_surface_dib
 {
     HBITMAP DIBsection;
-    void *bitmap_data;
     UINT bitmap_size;
 };
 
@@ -2296,7 +2295,6 @@ struct wined3d_surface
     struct wined3d_resource resource;
     const struct wined3d_surface_ops *surface_ops;
     struct wined3d_texture *container;
-    void *user_memory;
 
     DWORD flags;
 
